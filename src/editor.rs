c729ea80ab877c191f575e79b25aa2af2331f511
Andrew Chin <achin@eminence32.net>
2018-05-19 22:58:33 -0400

    Initial commit

---
+++ src/editor.rs
@@ -0,0 +1,143 @@
+use term;
+use rustyline;
+use ::std::path::Path;
+use ::std::io::stdin;
+
+
+pub struct SimpleEditor {
+    vars: Vec<String>
+}
+
+impl SimpleEditor {
+    pub fn new(vars: Vec<String>) -> SimpleEditor {
+        SimpleEditor { vars }
+    }
+    pub fn run(mut self) -> Option<Vec<String>> {
+        // we have 2 readline editors, one for path inputs, and one for everything else
+
+        let mut readline = rustyline::Editor::<()>::with_config(rustyline::Config::builder()
+            .auto_add_history(false)
+            .build());
+
+        let mut path_readline = rustyline::Editor::with_config(rustyline::Config::builder()
+            .auto_add_history(true)
+            .build());
+
+        let c = rustyline::completion::FilenameCompleter::new();
+        path_readline.set_helper(Some((c, ())));
+
+
+        let mut msg: Option<&'static str> = None;
+
+        loop {
+            eprintln!();
+            eprintln!();
+
+            for (idx, var) in self.vars.iter().enumerate() {
+                let path = Path::new(&var);
+                let flags = match path.exists() {
+                    true => "",
+                    false => "del"
+                };
+                eprintln!("{: <5} {: >2}. {}", flags, idx, var);
+            }
+
+            if let Some(m) = msg {
+                eprintln!();
+                eprintln!("{}", m);
+                msg.take();
+            }
+
+            eprintln!();
+            if self.vars.len() > 0 {
+                eprintln!("Edit/move entry: 0-{}.  New entry: n.  Save: s.  Quit: q", self.vars.len() - 1);
+            } else {
+                eprintln!("New entry: n.  Quit: q");
+
+            }
+
+            match readline.readline("> ") {
+                Ok(s) => match s.trim() {
+                    "q" => { break }
+                    "s" => { return Some(self.vars) }
+                    "n" => {
+                        eprintln!("Enter new path (or just press enter to cancel)");
+                        match path_readline.readline(">> ") {
+                            Ok(new_path) => {
+                                {
+                                    let path = Path::new(&new_path);
+                                    if !path.exists() {
+                                        msg = Some("Warning: this path doesn't exist!");
+                                    }
+                                }
+                                self.vars.push(new_path);
+                            }
+                            Err(e) => {
+                                eprintln!("{:?}", e);
+                                msg = Some("Nevermind, not adding any new paths...");
+                            }
+                        }
+                    }
+                    x => {
+                        if let Ok(num) = usize::from_str_radix(x, 10) {
+                            if num >= self.vars.len() {
+                                msg = Some("That number is not valid.");
+                            } else {
+                                {
+                                    let path = Path::new(&self.vars[num]);
+                                    eprintln!();
+                                    eprintln!("Editing {}", path.display());
+                                    if !path.exists() {
+                                        eprintln!("Warning: this path doesn't exist.");
+                                    }
+                                }
+
+                                eprintln!("Delete: d.  Edit value: e.  Set new position: 0-{} or b for bottom", self.vars.len() - 1);
+                                match readline.readline(">> ") {
+                                    Ok(s) => match s.as_ref() {
+                                        "d" => {
+                                            self.vars.remove(num);
+                                            msg = Some("Entry has been deleted.");
+                                        }
+                                        "e" => {
+                                            match path_readline.readline_with_initial("edit> ", (&self.vars[num].as_ref(), "")) {
+                                                Ok(edited_path) => {
+                                                    self.vars[num] = edited_path.trim().to_owned();
+                                                }
+                                                Err(..) => {
+                                                    msg = Some("Nevermind, not editing that path");
+                                                }
+                                            };
+                                        }
+                                        "b" => {
+                                            let entry = self.vars.remove(num);
+                                            self.vars.push(entry);
+                                            msg = Some("Entry has been moved.");
+                                        }
+                                        x => {
+                                            if let Ok(new_position) = usize::from_str_radix(x, 10) {
+                                                let entry = self.vars.remove(num);
+                                                self.vars.insert(new_position, entry);
+                                                msg = Some("Entry has been moved.");
+                                            } else {
+                                                msg = Some("That number is not valid.")
+                                            }
+                                        }
+                                    }
+//
+                                    Err(..) => {}
+                                }
+                            }
+                        } else {
+                            msg = Some("That option was not recognized.")
+                        }
+                    }
+                },
+                Err(..) => break
+            }
+        }
+
+        None
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+extern crate setenv;
+extern crate clap;
+extern crate term;
+extern crate rustyline;
+
+#[cfg(windows)]
+extern crate winapi;
+
+use std::env;
+
+use clap::{Arg, App};
+
+mod editor;
+
+
+fn main() {
+    let shell = setenv::get_shell();
+
+    let matches = App::new("My Super Program")
+        .arg(Arg::with_name("sep")
+            .short("s")
+            .long("separator")
+            .takes_value(true)
+            .default_value(if cfg!(windows) { ";" } else { ":" })
+            .help("Separator character"))
+        .arg(Arg::with_name("shell")
+            .long("shell")
+            .takes_value(true)
+            .help(&format!("What shell to use (autodetected as {:?})", shell.get_name()))
+        )
+        .arg(Arg::with_name("var")
+            .required(true)
+            .help("Environment variable to edit"))
+        .get_matches();
+
+    let var_name = matches.value_of("var").unwrap(); // not panicing, as clap ensure a value
+
+    let sep_char = matches.value_of("sep").unwrap(); // not panicing, as clap ensures a value
+
+    let path_list = if let Ok(path_val) = env::var(var_name) {
+        path_val.split(sep_char).filter(|x| !x.is_empty()).map(|x| x.to_owned()).collect()
+    } else {
+        eprintln!("Variable is not defined, starting with an empty list");
+        Vec::new()
+    };
+
+    let editor = editor::SimpleEditor::new(path_list);
+    if let Some(v) = editor.run() {
+        let new_val = v.join(sep_char);
+        shell.setenv(var_name, &new_val);
+        eprintln!();
+        eprintln!("{} environment variable updated", var_name);
+    } else {
+        eprintln!();
+        eprintln!("{} environment variable unchanged.", var_name);
+    }
+}
